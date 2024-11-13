86aff40e0da228c952cc1c10ac76f11d8914109a
huangxc_233 <huangxc_233@qq.com>
2024-11-13 18:32:15 +0800

    修改了BlockWidget文件夹下的文件结构，这是为了接下来把每个块分成多个cpp文件实现

--- Srcs/EntryWidget/BlockWidget/BlockWidgets.h
+++ Srcs/EntryWidget/BlockWidget/BlockWidgets.h
@@ -1,7 +1,7 @@
 ﻿// 该文件包含所有的BlockWidget的子类，外界调用时请直接include本文件
 
 #include "BlockWidget.h"
-#include "TextBlockWidget.h"
-#include "CodeBlockWidget.h"
-#include "HeaderBlockWidget.h"
-#include "ImageBlockWidget.h"
+#include "TextBlock/TextBlockWidget.h"
+#include "CodeBlock/CodeBlockWidget.h"
+#include "HeaderBlock/HeaderBlockWidget.h"
+#include "ImageBlock/ImageBlockWidget.h"
